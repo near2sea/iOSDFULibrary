2459a2a98e0fdd46cb9f3376a8b32eb904b1de76
lihaijian <sanan.li@qq.com>
2020-07-12 12:22:08 +0800

    初始化

---
+++ iOSDFULibrary/iOSDFULibrary-umbrella.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+#import "IntelHex2BinConverter.h"
+
+FOUNDATION_EXPORT double iOSDFULibraryVersionNumber;
+FOUNDATION_EXPORT const unsigned char iOSDFULibraryVersionString[];
---
+++ iOSDFULibrary/iOSDFULibrary.h
@@ -0,0 +1,19 @@
+//
+//  iOSDFULibrary.h
+//  iOSDFULibrary
+//
+//  Created by lihaijian on 2020/7/12.
+//  Copyright © 2020 lihaijian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSDFULibrary.
+FOUNDATION_EXPORT double iOSDFULibraryVersionNumber;
+
+//! Project version string for iOSDFULibrary.
+FOUNDATION_EXPORT const unsigned char iOSDFULibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSDFULibrary/PublicHeader.h>
+
+
